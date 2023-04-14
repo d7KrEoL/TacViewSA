f531691b59c1b6e29cf9fc23597b075083994826
d7KrEoL <74565655+d7KrEoL@users.noreply.github.com>
2023-04-14 16:34:09 +0300

    Required by TacViewsa.lua
    Usefull one by MstrVLT

---
+++ Client/lib/switch.lua
@@ -0,0 +1,24 @@
+--Tnx MstrVLT for this code, found it usefull for my project. I'll put this link to the original repository here: https://github.com/kitsunies/switch.lua
+local switch = {}
+
+switch.__index = switch
+
+switch.__call = function(self, v)
+    local c = self._callbacks[v] or self._default
+    assert(c, "No case statement defined for variable, and :default is not defined")
+    c()
+end
+
+function switch:case(v, f)
+    self._callbacks[v] = f
+    return self
+end
+
+function switch:default(f)
+    self._default = f
+    return self
+end
+
+return function() 
+    return setmetatable({_callbacks = {}}, switch) 
+end
